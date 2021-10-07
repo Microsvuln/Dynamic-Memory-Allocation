da979e3746bcb561fa5a16fd8275c9b92a071150
microsvuln <55649192+Microsvuln@users.noreply.github.com>
2021-10-07 13:33:25 +0330

    fix
    fix

--- 1.c
+++ 1.c
@@ -4,6 +4,6 @@ int main(int argc, char *argv[])
 	int *a;
 	int *arraySize;
 	scanf("%d", &arraySize);
-	a = (iny*)malloc(sizeof(int*) * arraySize);
+	a = (int*)malloc(sizeof(int*) * arraySize);
 	return 0;
 }
